1b583c6a61b2fe42932dba6f2c0d972dc7b62e87
niyor1 <niyorg1@gmail.com>
2025-02-24 16:49:09 +0000

    initalising the game state factory

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,14 +4,79 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
+	private final class MyGameState implements GameState {
+		// MyGameState class for implementing methods to update the gamestate by returning new gamestate
+
+		@Nonnull
+		@Override
+		public GameState advance(Move move) {
+			// moves will be either single or double move depending on that the travel log has to be updated
+			return move.accept(new Move.Visitor<GameState>() {
+				@Override
+				public GameState visit(Move.SingleMove move) {
+					return null;
+				}
+
+				@Override
+				public GameState visit(Move.DoubleMove move) {
+					return null;
+				}
+			});
+		}
+
+		@Nonnull
+		@Override
+		public GameSetup getSetup() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getPlayers() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+	}
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
